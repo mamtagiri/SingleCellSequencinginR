054c648cbb6cc152ec685dbdd68fca359f9ca970
mamtagiri <44902002+mamtagiri@users.noreply.github.com>
2020-05-15 11:21:50 -0400

    Update SeuratFunctions.R

--- SeuratFunctions.R
+++ SeuratFunctions.R
@@ -26,6 +26,24 @@ DoHeatmap(
   disp.max = Inf
 )
 
+                
+#HISTOGRAM OF THE UMI, Genes and percent mitochondrial genes
+
+g<-YOUROBJECT@metadata 
+
+#Percent mitocondrial histo
+
+ggplot(g, aes(x=percent.mt)) + geom_histogram( binwidth=2, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+    ggtitle("Percentage Mitochondrial genes histo") 
+    
+# UMI count histo    
+ggplot(g, aes(x=nCount_RNA)) + geom_histogram( binwidth=2, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+    ggtitle("UMI count histo") 
+    
+# Gene count histo    
+ggplot(g, aes(x=nCount_features)) + geom_histogram( binwidth=2, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+    ggtitle("Total genes histo") 
+ 
 
 ##UMAP
 UMAPPlot(a,pt.size=0.7,label.size=13,repel=TRUE,label=TRUE)
